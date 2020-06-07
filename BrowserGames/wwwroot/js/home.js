792ea5b440f35b10a1a4d140f843a285742b87a9
elaimaz <fariawesley4@gmail.com>
2020-06-06 21:59:53 -0300

    Main Commit, added ghost animation in the begiining.

---
+++ BrowserGames/wwwroot/js/home.js
@@ -0,0 +1,33 @@
+﻿//canvas context
+let canvas = document.getElementById("mainScreen");
+let context = canvas.getContext('2d');
+
+//image
+let ghostImage = document.getElementById('ghostImg');
+
+let lastTime = 0;
+
+let xPosition = 200;
+let speed = 5;
+
+function animationLoop(timeStamp) {
+    let deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+
+    if (xPosition > 970) {
+        speed = -5;
+    } else if (xPosition < 0) {
+        speed = 5;
+    }
+    //clear canvas
+    context.clearRect(0, 0, 1170, 400);
+
+    //drawImage
+    context.drawImage(ghostImage, xPosition, 100, 200, 200);
+
+    xPosition += speed;
+    console.log(xPosition);
+    requestAnimationFrame(animationLoop);
+}
+
+animationLoop();
